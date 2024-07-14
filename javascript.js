113bfea02efc9be5f1d1affedef32b369e103301
Akshaygohrava <63100064+Akshaygohrava@users.noreply.github.com>
2024-07-14 12:01:02 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+console.log("Hello, Akshay-Gohrava!");
+//for opening the Menu-bar
+function drop() {
+    let drop = document.getElementById('drop');
+    drop.style.width = '100%';
+}
+// for closing the Menu-bar
+function closed() {
+    let closed = document.getElementById('drop');
+    closed.style.width = '0';
+} 
+// menu dropping 
+function down() {
+    let down = document.getElementById('menu-down');
+    down.style.height = "100vh";
+}
+// closing the menu
+function xclose() {
+    const xclose = document.getElementById('menu-down');
+    xclose.style.height = "0";
+}
